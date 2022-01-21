a2e44883a358500b8746186820d642c3a875b692
mvpyb <454539387@qq.com>
2022-01-21 10:22:10 +0800

    add hot

--- src/index.js
+++ src/index.js
@@ -15,6 +15,6 @@ const render = () => {
 
 render()
 
-// if (process.env.NODE_ENV === 'development' && module.hot) {
-//   module.hot.accept('./App', render)
-// }
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./App', render)
+}
