aff2fe443428f6710f1d7118574f49b87b666c3d
mvpyb <454539387@qq.com>
2022-01-21 14:29:57 +0800

    add screenfull

---
+++ src/components/FullScreen/index.jsx
@@ -0,0 +1,43 @@
+
+import React, { useState, useEffect } from 'react'
+import screenFull from 'screenfull'
+import { message, Tooltip } from 'antd'
+import { FullscreenOutlined, FullscreenExitOutlined } from '@ant-design/icons'
+import './index.less'
+
+const FullScreen = () => {
+  const [isFullscreen, setIsFullscreen] = useState( false )
+  const isEnabled = screenFull.isEnabled
+
+  const change = () => {
+    setIsFullscreen( screenFull.isFullscreen )
+  }
+
+  const toggleScreen = () => {
+    if ( !isEnabled ) {
+      message.warning( '你的浏览器不支持，请更换高版本浏览器' )
+      return false
+    }
+    screenFull.toggle()
+  }
+
+  useEffect( () => {
+    isEnabled && screenFull.on( 'change', change )
+    return () => {
+      isEnabled && screenFull.off( 'change', change )
+    }
+  }, [] )
+
+  const title = isFullscreen ? '取消全屏' : '全屏'
+  return (
+    <div className='fullScreen-section'>
+      <Tooltip placement='bottom' title={title}>
+        {
+          isFullscreen ? <FullscreenExitOutlined onClick={toggleScreen} /> : <FullscreenOutlined onClick={toggleScreen} />
+        }
+      </Tooltip>
+    </div>
+  )
+}
+
+export default FullScreen
