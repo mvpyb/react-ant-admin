522ab4bfc9b7514ed3200dad7fb53525e5903cae
mvpyb <454539387@qq.com>
2022-01-21 18:27:04 +0800

    Create index.jsx

---
+++ src/layouts/components/sidebar/index.jsx
@@ -0,0 +1,30 @@
+
+import React from 'react'
+import { connect } from 'react-redux'
+import { Layout } from 'antd'
+import Logo from './Logo'
+import Menu from './Menu'
+const { Sider } = Layout
+
+const LayoutSider = ( props ) => {
+  const { sidebarCollapsed, sidebarLogo } = props
+  return (
+    <Sider
+      collapsible
+      collapsed={sidebarCollapsed}
+      trigger={null}
+      style={{ zIndex : '10' }}
+    >
+      {sidebarLogo ? <Logo /> : null}
+      <Menu />
+    </Sider>
+  )
+}
+
+const mapStateToProps = ( state ) => {
+  return {
+    ...state.app,
+    ...state.settings
+  }
+}
+export default connect( mapStateToProps )( LayoutSider )
