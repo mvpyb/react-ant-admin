dbfc088e7ac29c1ec16670f309c3eed122a1dd29
mvpyb <454539387@qq.com>
2022-01-21 14:30:31 +0800

    Update 404.jsx

--- src/views/errorPage/404.jsx
+++ src/views/errorPage/404.jsx
@@ -16,21 +16,21 @@ const Page404 = ( props ) => {
   }
   return (
     <DocumentTitle title={'404'}>
-      <div className="wscn-http404-container">
-  
-        <div className="wscn-http404">
-          <div className="pic-404">
-            <img className="pic-404__parent" src={img404} alt="404"/>
-            <img className="pic-404__child left" src={cloud} alt="404"/>
-            <img className="pic-404__child mid" src={cloud} alt="404"/>
-            <img className="pic-404__child right" src={cloud} alt="404"/>
+      <div className='wscn-http404-container'>
+
+        <div className='wscn-http404'>
+          <div className='pic-404'>
+            <img className='pic-404__parent' src={img404} alt='404'/>
+            <img className='pic-404__child left' src={cloud} alt='404'/>
+            <img className='pic-404__child mid' src={cloud} alt='404'/>
+            <img className='pic-404__child right' src={cloud} alt='404'/>
           </div>
-          <div className="bullshit">
-            <div className="bullshit__oops">404!</div>
-            <div className="bullshit__info">版权所有</div>
-            <div className="bullshit__headline">您访问的页面不见了...</div>
-            <div className="bullshit__info">请检查您输入的网址是否正确，或者点击下面的按钮返回首页。</div>
-            <a href="#!" className="bullshit__return-home" onClick={backToHome}>返回首页</a>
+          <div className='bullshit'>
+            <div className='bullshit__oops'>404!</div>
+            <div className='bullshit__info'>版权所有</div>
+            <div className='bullshit__headline'>您访问的页面不见了...</div>
+            <div className='bullshit__info'>请检查您输入的网址是否正确，或者点击下面的按钮返回首页。</div>
+            <a href='#!' className='bullshit__return-home' onClick={backToHome}>返回首页</a>
           </div>
         </div>
       </div>
