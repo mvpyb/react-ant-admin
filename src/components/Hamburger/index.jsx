5da20fa5a81d998b11093726f02537704741f2b4
mvpyb <454539387@qq.com>
2022-01-21 14:30:11 +0800

    add Hamburger

---
+++ src/components/Hamburger/index.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react'
+import { connect, useDispatch } from 'react-redux'
+import './index.less'
+
+import { TOGGLE_SIDEBAR } from '@store/reducers/app'
+import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons'
+
+const Hamburger = ( props ) => {
+  const { sidebarStatus } = props
+  const dispatch = useDispatch()
+
+  const toggleClick = () => {
+    dispatch( TOGGLE_SIDEBAR() )
+  }
+
+  return (
+    <div className='hamburger-section' onClick={toggleClick}>
+      {
+        sidebarStatus ? <MenuFoldOutlined /> : <MenuUnfoldOutlined />
+      }
+    </div>
+  )
+}
+
+export default connect( ( state ) => state.app )( Hamburger )
