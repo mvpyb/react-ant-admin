d831eaaa5908cc3fb1368f4a8ab875179d1a7d7f
mvpyb <454539387@qq.com>
2022-01-21 13:48:35 +0800

    Update users.js

--- src/store/reducers/users.js
+++ src/store/reducers/users.js
@@ -1,14 +1,13 @@
 
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { getCookie, addCookie, removeCookie } from '@utils/cookies'
-import { COOKIE_PREFIX, TOKEN } from '@config/constant'
-import { getUserInfo } from '@api/user'
+import { getUserInfo, logOut } from '@api/user'
 
 export const initialState = {
   username : '',
   roles : [],
   avatar : '',
-  token : getCookie( TOKEN ) || ''
+  token : getCookie( 'token' ) || ''
 }
 
 // A slice for posts with our three reducers
@@ -18,25 +17,25 @@ export const userSlice = createSlice( {
   reducers : {
     setToken : ( state, { payload : token } ) => {
       state.token = token || ''
-      addCookie( TOKEN, token )
+      addCookie( 'token', token )
     },
     setUserInfo : ( state, { payload : { roles, avatar, username }} ) => {
       state.roles = roles
       state.avatar = avatar || ''
       state.username = username || ''
 
-      addCookie( `${COOKIE_PREFIX}username`, username )
-      addCookie( `${COOKIE_PREFIX}roles`, roles )
+      addCookie( `username`, username )
+      addCookie( `roles`, roles )
     },
     clearUserInfo : ( state ) => {
       state.roles = []
       state.avatar = ''
       state.username = ''
       state.token = ''
-
-      removeCookie( `${COOKIE_PREFIX}username` )
-      removeCookie( `${COOKIE_PREFIX}roles` )
-      removeCookie( `${COOKIE_PREFIX}token` )
+      
+      removeCookie( 'username' )
+      removeCookie( 'roles' )
+      removeCookie( 'token' )
     }
   },
 
@@ -75,32 +74,51 @@ export const asyncGetInfo = payload => async( dispatch ) => {
 
 // 文档 ： https://redux-toolkit.js.org/api/createAsyncThunk
 export const getUserInfoSlice = createAsyncThunk(
-  'user/getUserInfo',
-  async( params, thunkAPI ) => {
-
-    try {
-      const { code, data } = await getUserInfo( { token : getCookie( TOKEN ) || thunkAPI.getState().users.token } )
-      if ( code == 200 ) {
-        const result = {
-          username : data.username || data.nickName || data.phone,
-          avatar : data.avatar || '',
-          roles : ['admin']
+    'user/getUserInfo',
+    async( params, thunkAPI ) => {
+      
+      try {
+        const { code, data } = await getUserInfo( { token : getCookie( 'token' ) || thunkAPI.getState().users.token } )
+        if ( code == 200 ) {
+          const result = {
+            username : data.username || data.nickName || data.phone,
+            avatar : data.avatar || '',
+            roles : ['admin']
+          }
+          
+          thunkAPI.dispatch( setUserInfo( result ) )
+          
+          return result
+        } else {
+          thunkAPI.dispatch( clearUserInfo( ) )
+          return thunkAPI.rejectWithValue( data.message || '登录失败' )
+          // return Promise.reject( '登录请求错误' )
         }
-
-        thunkAPI.dispatch( setUserInfo( result ) )
-
-        return result
-      } else {
+      } catch ( err ) {
+        console.log( 'err', err )
         thunkAPI.dispatch( clearUserInfo( ) )
-        return thunkAPI.rejectWithValue( data.message || '登录失败' )
-        // return Promise.reject( '登录请求错误' )
+        return thunkAPI.rejectWithValue( err.response.data || '登录失败' )
       }
-    } catch ( err ) {
-      console.log( 'err', err )
-      thunkAPI.dispatch( clearUserInfo( ) )
-      return thunkAPI.rejectWithValue( err.response.data || '登录失败' )
     }
-  }
+)
+
+export const loginOut = createAsyncThunk(
+    'user/loginOut',
+    async( params, thunkAPI ) => {
+      
+      try {
+        const { code, data } = await logOut()
+        if ( code == 200 ) {
+          thunkAPI.dispatch( clearUserInfo() )
+          
+          return {}
+        } else {
+          return thunkAPI.rejectWithValue( data.message || '登出失败' )
+        }
+      } catch ( err ) {
+        return thunkAPI.rejectWithValue( err.response.data || '登出失败' )
+      }
+    }
 )
 
 export default userSlice.reducer
