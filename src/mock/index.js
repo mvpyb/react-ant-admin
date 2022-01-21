2c401040df46c9a1ebf2ae9cb9209075f4979a98
mvpyb <454539387@qq.com>
2022-01-21 13:49:10 +0800

    Update Mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,12 +2,13 @@
 import Mock from "mockjs"
 import loginMock from "./login"
 
-const { login, userInfo } = loginMock
+const { login, userInfo, logout } = loginMock
 
 // login
 Mock.mock( /\/login/, "post", login )
 
 // user
 Mock.mock( /\/userInfo/, "post", userInfo )
+Mock.mock( /\/logout/, "post", logout )
 
 export default Mock
