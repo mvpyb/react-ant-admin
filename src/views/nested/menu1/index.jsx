ed747437c30037f7a8b2747ca6e273431816c8ef
mvpyb <454539387@qq.com>
2022-02-16 14:02:55 +0800

    add nested

---
+++ src/views/nested/menu1/index.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Menu1 = () => {
+  return (
+    <div>
+      <h1>Menu1</h1>
+      <hr/>
+      <Outlet />
+    </div>
+  )
+}
+
+export default Menu1
---
+++ src/views/nested/menu1/menu1-2/index.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Menu1_2 = () => {
+  return (
+    <div>
+      <h1>Menu1-2 </h1>
+      <hr/>
+      <Outlet />
+    </div>
+  )
+}
+
+export default Menu1_2
