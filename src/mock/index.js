0c36cb10e24f8217152e87dcfed60ffbb92af5d4
mvpyb <454539387@qq.com>
2022-02-17 17:55:58 +0800

    Update index.js

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,8 +1,10 @@
 
 import Mock from "mockjs"
 import loginMock from "./login"
+import tableMock from "./table"
 
 const { login, userInfo, logout } = loginMock
+const { tableData } = tableMock
 
 // login
 Mock.mock( /\/login/, "post", login )
@@ -10,5 +12,6 @@ Mock.mock( /\/login/, "post", login )
 // user
 Mock.mock( /\/userInfo/, "post", userInfo )
 Mock.mock( /\/logout/, "post", logout )
+Mock.mock( /\/table\/list/, "get", tableData )
 
 export default Mock
