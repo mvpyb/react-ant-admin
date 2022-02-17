c23bd1774c869f10d8ea2e7347fa6411fc2b4850
mvpyb <454539387@qq.com>
2022-02-17 17:56:21 +0800

    Create tinymce.jsx

---
+++ src/views/components-demo/richText/tinymce.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react'
+import RichTextEditor from '@/components/RichText/tynimce'
+import { Alert } from 'antd'
+
+const TitleContent = () => {
+  return <p> 此页面用到的富文本编辑器是 <a href='https://www.tiny.cloud/docs/quick-start/'>Tinymce</a>。</p>
+}
+
+const RichTextTinymce = () => {
+  const editoInput = ( value ) => {
+    // console.log( 'editoInput', value )
+  }
+
+  const value = '<p> 此页面用到的富文本编辑器是 <a href=\'https://www.tiny.cloud/docs/quick-start/\'>Tinymce</a>。</p>'
+
+  return (
+    <div className='app-container'>
+      <Alert message={ <TitleContent /> } />
+      <br/>
+      <RichTextEditor value={value} editoInput={editoInput} />
+    </div>
+  )
+}
+
+export default RichTextTinymce
