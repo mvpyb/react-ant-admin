cc10b2cd023a65e9283628441bcfe63b3ad8ed0b
mvpyb <454539387@qq.com>
2022-02-17 17:55:05 +0800

    Create toolbar.js

---
+++ src/components/RichText/tynimce/toolbar.js
@@ -0,0 +1,23 @@
+
+/**
+ * alignjustify 两端对齐
+ * copy 复制
+ * cut 剪辑
+ * fontselect 字体样式选择
+ * fullscreen
+ * fontsizeselect 字体大小选择
+ * link 在编辑器中创建/编辑链接。
+ * openlink 在新选项卡中打开选定的链接。
+ *
+ * 首行缩进 https://segmentfault.com/a/1190000020987256?utm_source=tag-newest
+ * 首行缩进 http://tinymce.ax-z.cn/more-plugins/indent2em.php
+ * 文字间距 http://tinymce.ax-z.cn/more-plugins/more-index.php
+ * */
+
+const toolbar = `
+  undo redo removeformat fontsizeselect bold italic underline strikethrough forecolor backcolor
+ alignleft aligncenter alignright alignjustify indent2em | outdent indent | lineheight letterspacing numlist bullist |
+ ltr rtl image media blockquote table hr codesample checklist colorpicker
+`
+
+export default toolbar
