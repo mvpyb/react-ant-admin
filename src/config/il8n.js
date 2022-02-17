7d7bfec58f3b8aa1672dbf1d6d28ce4553a1359f
mvpyb <454539387@qq.com>
2022-02-17 17:55:17 +0800

    Update il8n.js

--- src/config/il8n.js
+++ src/config/il8n.js
@@ -5,7 +5,7 @@ import { initReactI18next } from 'react-i18next'
 // import Backend from 'i18next-http-backend'
 // import LanguageDetector from 'i18next-browser-languagedetector'
 
-import lang from '@/locales'
+import lang from '@/lang'
 
 i18n
 // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
