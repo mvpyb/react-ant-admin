beaae942af73a6ebca9e319b087b1a439e7d5ad7
mvpyb <454539387@qq.com>
2022-02-22 15:06:44 +0800

    Update index.jsx

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,16 +1,20 @@
 
 import React from 'react'
+import { connect } from 'react-redux'
 import { Layout } from 'antd'
 
 import NavBar from './components/navbar'
 import Main from './components/main'
 import SlideBar from './components/sidebar'
+import TagsView from './components/tagsView'
 
 import './index.less'
 
 import { Scrollbars } from 'react-custom-scrollbars'
 
 const BaseLayout = ( props ) => {
+  console.log( 'props', props )
+  const { tagsView } = props
   return (
     <Layout style={{ minHeight : '100vh' }}>
       <SlideBar />
@@ -25,6 +29,7 @@ const BaseLayout = ( props ) => {
           universal={false}
         >
           <NavBar />
+          {tagsView ? <TagsView /> : null}
           <Main />
         </Scrollbars>
       </Layout>
@@ -33,4 +38,4 @@ const BaseLayout = ( props ) => {
   )
 }
 
-export default BaseLayout
+export default connect( ( state ) => state.settings )( BaseLayout )
