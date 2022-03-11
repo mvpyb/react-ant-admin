bcc231bd16018f3886dd70b8d29eb209d4b58948
mvpyb <454539387@qq.com>
2022-03-11 16:45:53 +0800

    Update plugins.js

--- src/components/RichText/tynimce/plugins.js
+++ src/components/RichText/tynimce/plugins.js
@@ -6,13 +6,13 @@ const plugins = [
   'advlist', // 高级插件列表 依赖lists插件
   'anchor', // 锚点插件
   'autolink', // 自动链接插件 - 当用户输入有效的完整URL时，autolink插件会自动创建超链接
-  // 'autoresize ', // TODO 开启之后设置height 无效 编辑器大小自适应 - 该插件提供自动调整编辑器大小的方法以适应内容
+  // 'autoresize ', // 开启之后设置height 无效 编辑器大小自适应 - 该插件提供自动调整编辑器大小的方法以适应内容
   'autosave', // 功能：定时保存内容，在文件菜单中添加了一个“恢复草稿”选项，在工具栏添加一个“恢复草稿”的可选按钮，同时如果用户修改了编辑区内的原始内容，在跳转URL之前，其还会弹出一个提示框，提醒用户修改的内容没有提交
   'charmap ', // 特殊字符插件
   'codesample', // 代码示例插件
   'directionality', // 适应不同语言的书写方式，ltr文字方向从左到右，rtl从右到左
   'emoticons', // 表情插件
-  // 'fullpage', // TODO 不用 文档属性 编辑元数据和文档属性，包含title，keywords，description和文档编码charset。
+  // 'fullpage', // 不用 文档属性 编辑元数据和文档属性，包含title，keywords，description和文档编码charset。
   'fullscreen', // 全屏
   'help', // 帮助
   'image', // 插入编辑图片
@@ -22,7 +22,7 @@ const plugins = [
   'nonbreaking', // 插入不间断空格
   'noneditable', // 不可编辑元素
   'pagebreak', // 插入分页符
-  'paste', // TODO 强大 粘贴插件 此插件将过滤/清除从Word粘贴过来的内容。它的强大之处在于它提供的强大配置，所以请花点时间将本页所有内容阅读完。
+  'paste', // 强大 粘贴插件 此插件将过滤/清除从Word粘贴过来的内容。它的强大之处在于它提供的强大配置，所以请花点时间将本页所有内容阅读完。
   'quickbars', // 快速工具栏
   'save', // 在工具栏添加了一个保存提交按钮，点击它将提交编辑器所在的表单
   'searchreplace', // 查找替换
@@ -32,7 +32,7 @@ const plugins = [
   // toolbar中可配置这些:tabledelete | tableprops tablerowprops tablecellprops |
   // tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol
   'table',
-  'template', // TODO 强大 内容模板
+  'template', // 强大 内容模板
   'textcolor', // 文字颜色
   'textpattern', // 快速排版（类似markdown）
   'toc', // 目录生成器
