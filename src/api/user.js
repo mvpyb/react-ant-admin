2d4c74b8b57864ffdea9b2767b015d54eb310ef2
bing.yu <yu.bing@mail.com>
2023-05-16 17:37:01 +0800

    Update user.js

--- src/api/user.js
+++ src/api/user.js
@@ -1,19 +1,31 @@
 
 import { request } from '@/utils/request'
 
-export function getUserInfo( data ) {
-  return request( {
-    // url : '/user/tokenLogin',
-    url : '/userInfo',
-    method : 'post',
-    data
-  } )
+export function getUserInfo(data) {
+  return Promise.resolve({
+    code: 200,
+    message: 'success',
+    data: {
+      id: 'admin',
+      roles: ['admin'],
+      username: '灰是小灰灰的灰',
+      avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80',
+      description: '我是超级管理员'
+    }
+  })
+
+  // return request( {
+  //   // url : '/user/tokenLogin',
+  //   url : '/userInfo',
+  //   method : 'post',
+  //   data
+  // } )
 }
 
-export function logOut( data ) {
-  return request( {
-    url : '/logout',
-    method : 'post',
+export function logOut(data) {
+  return request({
+    url: '/logout',
+    method: 'post',
     data
-  } )
+  })
 }
