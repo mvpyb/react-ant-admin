7dbc37c19dea70e50637f8b8779d3d2aa71c1d51
bing.yu <yu.bing@mail.com>
2023-05-16 17:36:56 +0800

    Update login.js

--- src/api/login.js
+++ src/api/login.js
@@ -1,19 +1,18 @@
 
 import { request } from '@/utils/request'
 
-// 此处接入的是真实接口
-// export function login( data ) {
-//   return request( {
-//     method : 'post',
-//     url : '/user/accountLogin',
-//     data
-//   } )
-// }
+export function login(data) {
+  return Promise.resolve({
+    code: 200,
+    message: 'success',
+    data: {
+      token: 'admin-token'
+    }
+  })
 
-export function login( data ) {
-  return request( {
-    url : '/login',
-    method : 'post',
-    data
-  } )
+  // return request( {
+  //   url : '/login',
+  //   method : 'post',
+  //   data
+  // } )
 }
